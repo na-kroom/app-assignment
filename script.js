9d9934f9b243df266fa59facb8583c5649ce350e
NANA <ohzono1237@cloud.com>
2026-05-28 22:04:20 +0900

    improve design

--- script.js
+++ script.js
@@ -2,40 +2,32 @@
 const today = new Date();
 
 const todayEl = document.getElementById("today");
-const daysEl = document.getElementById("days");
+const daysEl =document.getElementById("days");
 
-const eventNameEl =
-  document.getElementById("event-name");
+const eventNameEl = document.getElementById("event-name");
 
-const eventDateEl =
-  document.getElementById("event-date");
+const eventDateEl = document.getElementById("event-date");
 
-const nameInput =
-  document.getElementById("name-input");
+const nameInput = document.getElementById("name-input");
 
-const dateInput =
-  document.getElementById("date-input");
+const dateInput = document.getElementById("date-input");
 
-const button =
-  document.getElementById("set-btn");
+const button = document.getElementById("set-btn");
 
-todayEl.textContent =
-  today.toLocaleDateString();
+todayEl.textContent = today.toLocaleDateString();
 
 button.addEventListener("click", ()=> {
 
   // 入力値
   const eventName = nameInput.value;
 
-  const targetDate =
-    new Date(dateInput.value);
+  const targetDate = new Date(dateInput.value);
 
-  const diffTime =
-    targetDate - new Date();
+  const diffTime = targetDate - new Date();
 
   const diffDays =
     Math.ceil(
-      diffTime / (1000 * 60 * 60 * 24)
+      diffTime / (1000*60*60*24)
     );
 
   daysEl.textContent = diffDays;
